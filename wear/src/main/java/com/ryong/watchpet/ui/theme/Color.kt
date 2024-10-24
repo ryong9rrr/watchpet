666d3174f6b9bfcf04601f2581ce10dd0d5f8e0f
yong <tkddbs1644@gmail.com>
2024-10-24 22:39:14 +0900

    design(wear): add blackAlpha, gray, yellow, orange, teal color token

---
+++ wear/src/main/java/com/ryong/watchpet/ui/theme/Color.kt
@@ -0,0 +1,63 @@
+package com.ryong.watchpet.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// black-alpha
+val BlackAlpha50 = Color(0xFF000000).copy(alpha = 0.04f)
+val BlackAlpha100 = Color(0xFF000000).copy(alpha = 0.06f)
+val BlackAlpha200 = Color(0xFF000000).copy(alpha = 0.08f)
+val BlackAlpha300 = Color(0xFF000000).copy(alpha = 0.16f)
+val BlackAlpha400 = Color(0xFF000000).copy(alpha = 0.24f)
+val BlackAlpha500 = Color(0xFF000000).copy(alpha = 0.36f)
+val BlackAlpha600 = Color(0xFF000000).copy(alpha = 0.48f)
+val BlackAlpha700 = Color(0xFF000000).copy(alpha = 0.64f)
+val BlackAlpha800 = Color(0xFF000000).copy(alpha = 0.80f)
+val BlackAlpha900 = Color(0xFF000000).copy(alpha = 0.92f)
+
+// gray
+val Gray50 = Color(0xFFF7FAFC)
+val Gray100 = Color(0xFFEDF2F7)
+val Gray200 = Color(0xFFE2E8F0)
+val Gray300 = Color(0xFFCBD5E0)
+val Gray400 = Color(0xFFA0AEC0)
+val Gray500 = Color(0xFF718096)
+val Gray600 = Color(0xFF4A5568)
+val Gray700 = Color(0xFF2D3748)
+val Gray800 = Color(0xFF1A202C)
+val Gray900 = Color(0xFF171923)
+
+// orange
+val Orange50 = Color(0xFFFFFAF0)
+val Orange100 = Color(0xFFFEEBCB)
+val Orange200 = Color(0xFFFBD38D)
+val Orange300 = Color(0xFFF6AD55)
+val Orange400 = Color(0xFFED8936)
+val Orange500 = Color(0xFFDD6B20)
+val Orange600 = Color(0xFFC05621)
+val Orange700 = Color(0xFF9C4221)
+val Orange800 = Color(0xFF7B341E)
+val Orange900 = Color(0xFF652B19)
+
+// yellow
+val Yellow50 = Color(0xFFFFFFF0)
+val Yellow100 = Color(0xFFFEFCBF)
+val Yellow200 = Color(0xFFFAF089)
+val Yellow300 = Color(0xFFF6E05E)
+val Yellow400 = Color(0xFFECC94B)
+val Yellow500 = Color(0xFFD69E2E)
+val Yellow600 = Color(0xFFB7791F)
+val Yellow700 = Color(0xFF975A16)
+val Yellow800 = Color(0xFF744210)
+val Yellow900 = Color(0xFF5F370E)
+
+// teal
+val Teal50 = Color(0xFFE6FFFA)
+val Teal100 = Color(0xFFB2F5EA)
+val Teal200 = Color(0xFF81E6D9)
+val Teal300 = Color(0xFF4FD1C5)
+val Teal400 = Color(0xFF38B2AC)
+val Teal500 = Color(0xFF319795)
+val Teal600 = Color(0xFF2C7A7B)
+val Teal700 = Color(0xFF285E61)
+val Teal800 = Color(0xFF234E52)
+val Teal900 = Color(0xFF1D4044)
